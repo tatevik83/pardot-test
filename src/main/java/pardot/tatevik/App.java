f3f6ec23e4e4e694d87ad8b381820ab8e6c7d079
tatevik83 <tabovyan@salesforce.com>
2016-04-18 23:35:33 -0700

    Final changes

--- src/main/java/pardot/tatevik/App.java
+++ src/main/java/pardot/tatevik/App.java
@@ -1,13 +1,12 @@
 package pardot.tatevik;
 
 /**
- * Hello world!
+ * Application starts here
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        // TODO @tatevik Write app code
     }
 }
