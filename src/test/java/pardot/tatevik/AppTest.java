87268a94296cf09319b19116f37e7b43abd8fc48
tatevik83 <tabovyan@salesforce.com>
2016-04-18 23:31:04 -0700

    Adding comments

--- src/test/java/pardot/tatevik/AppTest.java
+++ src/test/java/pardot/tatevik/AppTest.java
@@ -18,14 +18,15 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * WebDriver test for pardot
  */
 public class AppTest extends TestCase
 {
 	private final String URL = "https://pi.pardot.com"; 
 	private final String LOGIN = "pardot.applicant@pardot.com";
 	private final String PASSWORD = "Applicant2012"; 
-	private final int TIMEOUT = 10000;
+	private final int LONG_PERIOD = 10000;
+	private final int SHORT_PERIOD = 2000;
 	
     /**
      * Create the test case
@@ -46,77 +47,101 @@ public class AppTest extends TestCase
     }
 
     /**
-     * Rigourous Test :-)
+     * WebDriver test for pardot
      */
-    public void testApp()
-    {
-    	WebDriver driver = null;
-    	
-    	try {
-    	
-    	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver");
-    	
-    	driver = new ChromeDriver(); 
-    	driver.get(URL);
-    	
-    	login(driver);
-    	
-    	
-    	String name = "11tatevik_" + UUID.randomUUID().toString();
-    	//String name = "tatevik_81b172e9-42cb-489c-a9ca-98b3fdc49c45";
-        
-        navigateToList(driver);
-        createList(driver, name, true);
-        
-        // repeat
-        navigateToList(driver);
-        createList(driver, name, false);
-        
-        //validate that validation fails and right error messages are displayed 
-        List<WebElement> errorElems = driver.findElements(By.cssSelector(".modal-dialog .alert-error")); 
-        Assert.assertTrue("Error messages printed should be 2", errorElems.size() == 2);
-        Assert.assertEquals("Unexpected error text", errorElems.get(0).getText(), "Please correct the errors below and re-submit");
-        Assert.assertEquals("Unexpected error text", errorElems.get(1).getText(), "Name\nPlease input a unique value for this field");
-        
-        // cancel and goto home for edit
-        driver.findElement(By.linkText("Cancel")).click();
-        driver.findElement(By.linkText(name)).click();
-        waitForHeader(driver, "h1", name);
-        editList(driver, name); 
-        
-        // this should be successful
-        // Exception will be thrown if something goes wrong
-        navigateToList(driver);
-        createList(driver, name, true);
+	public void testApp() {
+		WebDriver driver = null;
 
-        // create and validate the prospect
-        navigateToProspects(driver); 
-        createProspect(driver, name + "@gmail.com", name);
-        validateListAddedToProspect(driver, name);
-        
-        // create email
-        navigateToEmails(driver);
-        createEmail(driver, name);
+		try {
+			// Using CHROME WebDriver
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "/src/test/resources/chromedriver");
 
-        logout(driver);
-    	} catch (Exception x) {
-    		Assert.fail("Unexpected error occured: " + x.getMessage());
-    	}
-    	finally {
-    		if (driver != null) {
-    			driver.close();
-    			driver.quit();
-    		}
-    	}
-    }
+			driver = new ChromeDriver();
+			driver.get(URL);
+
+			login(driver); // login
+			
+			// starts with 11 so that the search for list with this name is not exhaustive 
+			String name = "11tatevik_" + UUID.randomUUID().toString();
+
+			// create the first list
+			navigateToList(driver);
+			createList(driver, name, true);
+
+			// repeat the process
+			navigateToList(driver);
+			createList(driver, name, false);
+
+			// verify that validation fails and right error messages aredisplayed
+			verifyErrorMessagesOnListSave(driver);
+
+			// cancel and goto record home for edit
+			driver.findElement(By.linkText("Cancel")).click();
+			driver.findElement(By.linkText(name)).click();
+			waitForHeader(driver, "h1", name);
+			editList(driver, name);
+
+			// if this line is reached then edit was successful
+			
+			// as the name is changed, the following create should still succeed
+			navigateToList(driver);
+			createList(driver, name, true);
+			
+			// if this line is reached then create was successful
+
+			// create and validate a prospect
+			navigateToProspects(driver);
+			createProspect(driver, name + "@gmail.com", name);
+			validateListAddedToProspect(driver, name);
+
+			// create email
+			navigateToEmails(driver);
+			createEmail(driver, name);
+
+			logout(driver); // logout
+		} catch (Exception x) {
+			Assert.fail("Test failed with unexpected error: " + x.getMessage());
+		} finally {
+			if (driver != null) {
+				driver.close();
+				driver.quit();
+			}
+		}
+	}
+	
+	/**
+	 * Verify that correct error messages are displayed when List is saved with duplicate name
+	 * 
+	 * @param driver CHROME WebDriver 
+	 */
+	private void verifyErrorMessagesOnListSave(WebDriver driver) {
+		List<WebElement> errorElems = driver.findElements(By.cssSelector(".modal-dialog .alert-error"));
+		Assert.assertTrue("Error messages printed should be 2", errorElems.size() == 2);
+		Assert.assertEquals("Unexpected error text", errorElems.get(0).getText(),
+				"Please correct the errors below and re-submit");
+		Assert.assertEquals("Unexpected error text", errorElems.get(1).getText(),
+				"Name\nPlease input a unique value for this field");
+	}
     
+	/**
+	 * Use web driver to login to pardot org
+	 * 
+	 * @param driver CHROME WebDriver
+	 */
     private void login(WebDriver driver) {
     	driver.findElement(By.id("email_address")).sendKeys(LOGIN);
         driver.findElement(By.id("password")).sendKeys(PASSWORD);     
         driver.findElement(By.name("commit")).submit();
     }
     
+    /**
+     * Use web driver to navigate to pardot's emails section
+     * 
+     * @param driver CHROME WebDriver
+     */
     private void navigateToEmails(WebDriver driver) {
+    	
     	Actions actions = new Actions(driver);
         WebElement menuHoverLink = driver.findElement(By.id("mark-tog"));
         actions.moveToElement(menuHoverLink).build().perform();
@@ -131,10 +156,15 @@ public class AppTest extends TestCase
         actions.click();
         actions.perform();
         
-        // wait for the header to turn to Lists
+        // wait for the header to turn to emails section header
         waitForHeader(driver, "h4", "Basic Email Information");
     }
     
+    /**
+     * Use web driver to logout from pardot org 
+     * 
+     * @param driver CHROME WebDriver
+     */
     private void logout(WebDriver driver) {
     	Actions actions = new Actions(driver);
         WebElement userIcon = driver.findElement(By.className("icon-user"));
@@ -145,6 +175,11 @@ public class AppTest extends TestCase
         actions.perform(); 
     }
     
+    /**
+     * Use web driver to navigate to the Prospects section of the org
+     * 
+     * @param driver CHROME WebDriver
+     */
     private void navigateToProspects(WebDriver driver) {
     	Actions actions = new Actions(driver);
         WebElement menuHoverLink = driver.findElement(By.id("pro-tog"));
@@ -158,6 +193,11 @@ public class AppTest extends TestCase
         
     }
     
+    /**
+     * Use web driver to navigate to the Lists section of the org
+     * 
+     * @param driver CHROME WebDriver
+     */
     private void navigateToList(WebDriver driver) {
     	Actions actions = new Actions(driver);
         WebElement menuHoverLink = driver.findElement(By.id("mark-tog"));
@@ -178,8 +218,13 @@ public class AppTest extends TestCase
         
     }
     
-    private void validateListAddedToProspect(WebDriver driver, String listName) throws InterruptedException {
-    	//waitForElement(driver, By.xpath("//a[contains(text(),'Lists')]"));
+    /**
+     * Use web driver to validate that given list is added to prospect that is currently displayed
+     * 
+     * @param driver CHROME WebDriver
+     * @param listName Name of the List 
+     */
+    private void validateListAddedToProspect(WebDriver driver, String listName) {
     	
     	List<WebElement> elems = driver.findElements(By.xpath("//a[contains(text(),'Lists')]")); 
     	for (WebElement element : elems) {
@@ -204,7 +249,13 @@ public class AppTest extends TestCase
     }
     
     
-    
+    /**
+     * Use web driver to create an email and add given list to it
+     * 
+     * @param driver CHROME WebDriver
+     * @param listName Name of the list to be added to the email
+     * @throws InterruptedException
+     */
     private void createEmail(WebDriver driver, String listName) throws InterruptedException {
     	driver.findElement(By.id("name")).sendKeys("Email from Tatevik");
     	
@@ -250,10 +301,18 @@ public class AppTest extends TestCase
     	driver.findElement(By.cssSelector("input[name='subject_a'][type='text']")).sendKeys("Email Subject");;
     	driver.findElement(By.id("save_footer")).click();
     	
-    	Thread.sleep(10000);
+    	sleep(); // Only here to see the email being saved
     	
     } 
     
+    /**
+     * Create a new prospect with given email and add given list to it
+     * 
+     * @param driver driver CHROME WebDriver
+     * @param email Prospect email
+     * @param listName List to add to prospect
+     * @throws InterruptedException
+     */
     private void createProspect(WebDriver driver, String email, String listName) throws InterruptedException {
     	driver.findElement(By.id("pr_link_create")).click();
         waitForElement(driver, By.id("email"));
@@ -294,17 +353,29 @@ public class AppTest extends TestCase
         
         waitForElement(driver, By.name("commit"));
         driver.findElement(By.name("commit")).submit();
-        Thread.sleep(2000);
+        sleep(); // TODO: @tatevik Find solution and remove. Here prevent weird behavior of Chrome WebDriver
         waitForHeader(driver, "h1", email);
         
     }
     
-    
+    /**
+     * Use web driver to select the first option for given HTML select element
+     * 
+     * @param driver CHROME WebDriver
+     * @param by By locator for HTML select element 
+     */
     private void selectFirstOption(WebDriver driver, By by) {
     	Select dropdown = new Select(driver.findElement(by));
     	dropdown.selectByIndex(1);
     }  
     
+    /**
+     * Use web driver to select given option for given HTML select element
+     * 
+     * @param driver CHROME WebDriver
+     * @param by By locator for HTML select element 
+     * @param optionName Option name
+     */
     private void selectOption (WebDriver driver, By by, String optionName) {
     	WebElement select = driver.findElement(by);
         List<WebElement> groups = select.findElements(By.tagName("optgroup"));
@@ -336,17 +407,30 @@ public class AppTest extends TestCase
         Assert.fail("Option " + optionName + " is missing from options");
     }
 
-    private void createList(WebDriver driver, String name, boolean wait) {
+    /**
+     * Use web driver to create List with given name
+     * 
+     * @param driver CHROME WebDriver
+     * @param name Name of the list 
+     * @param wait {@Boolean} indicator to wait and validate if record home is displayed
+     */
+    private void createList(WebDriver driver, String name, boolean validateHeader) {
         driver.findElement(By.id("listxistx_link_create")).click();
         waitForElement(driver, By.id("information_modal"));
 
         driver.findElement(By.id("name")).sendKeys(name);
         driver.findElement(By.id("save_information")).submit();
-        if (wait) {
+        if (validateHeader) {
         	waitForHeader(driver, "h1", name);
         }
     }
     
+    /**
+     * Use web driver to edit given list and append "_Edited" to its name
+     * 
+     * @param driver CHROME WebDriver
+     * @param name List name
+     */
     private void editList(WebDriver driver, String name) {
     	driver.findElement(By.linkText("Edit")).click();
     	waitForElement(driver, By.id("name")); 
@@ -357,13 +441,35 @@ public class AppTest extends TestCase
         waitForHeader(driver, "h1", name + "_Edited");
     }
     
+    /**
+     * Use web driver to wait until header with given type and text is displayed
+     * 
+     * @param driver CHROME WebDriver
+     * @param headerType Type of the header to wait for, e.g h1
+     * @param headerText Test of the header to wait for
+     */
     private void waitForHeader(WebDriver driver, String headerType, String headerText) {
-    	WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
+    	WebDriverWait wait = new WebDriverWait(driver, LONG_PERIOD);
         wait.until(ExpectedConditions.attributeContains(driver.findElement(By.cssSelector(headerType)), "innerHTML", headerText));
     }
     
+    /**
+     * Use web driver to wait until HTML element with given locator is displayed
+     * 
+     * @param driver CHROME WebDriver
+     * @param by By locator for given HTML element 
+     */
     private void waitForElement(WebDriver driver, By by) {
-    	WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
+    	WebDriverWait wait = new WebDriverWait(driver, LONG_PERIOD);
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+    
+    /**
+     * Introduce a short wait 
+     * 
+     * @throws InterruptedException
+     */
+    private void sleep() throws InterruptedException {
+    	Thread.sleep(SHORT_PERIOD);
     }
 }
